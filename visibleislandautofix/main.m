d0100ebe42948ccea70c3cd99dd1e70ecabf58d6
cihuasou <896033479@qq.com>
2026-08-19 15:40:28 +0800

    Update main.m

--- visibleislandautofix/main.m
+++ visibleislandautofix/main.m
@@ -1,18 +1,11 @@
 #import <Foundation/Foundation.h>
-#import <roothide.h>
-#import <unistd.h>
-
-static NSString *mobileGestaltPath(void)
-{
-    return jbroot(
-        @"/var/containers/Shared/SystemGroup/"
-         @"systemgroup.com.apple.mobilegestaltcache/"
-         @"Library/Caches/"
-         @"com.apple.MobileGestalt.plist"
-    );
-}
 
 int main(int argc, char *argv[])
 {
+    @autoreleasepool {
+        // MobileGestalt 的修改已经由 VisibleIsland Tweak 负责。
+        // LaunchDaemon 不再执行任何修复操作。
+    }
+
     return 0;
 }
