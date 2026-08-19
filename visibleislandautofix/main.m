90dfc125f850ba88705f62857d2e051c21a004f1
cihuasou <896033479@qq.com>
2026-08-19 15:28:52 +0800

    Update main.m

--- visibleislandautofix/main.m
+++ visibleislandautofix/main.m
@@ -12,69 +12,7 @@ static NSString *mobileGestaltPath(void)
     );
 }
 
-static BOOL enableDynamicIsland(void)
-{
-    NSString *path = mobileGestaltPath();
-
-    if (!path) {
-        return NO;
-    }
-
-    NSMutableDictionary *plist =
-        [NSMutableDictionary dictionaryWithContentsOfFile:path];
-
-    if (!plist) {
-        return NO;
-    }
-
-    NSMutableDictionary *cacheExtra =
-        [plist[@"CacheExtra"] mutableCopy];
-
-    if (!cacheExtra) {
-        return NO;
-    }
-
-    NSMutableDictionary *deviceInfo =
-        [cacheExtra[@"oPeik/9e8lQWMszEjbPzng"] mutableCopy];
-
-    if (!deviceInfo) {
-        return NO;
-    }
-
-    NSNumber *current =
-        deviceInfo[@"ArtworkDeviceSubType"];
-
-    if ([current intValue] == 2556) {
-        return YES;
-    }
-
-    deviceInfo[@"ArtworkDeviceSubType"] = @2556;
-
-    cacheExtra[@"oPeik/9e8lQWMszEjbPzng"] = deviceInfo;
-    plist[@"CacheExtra"] = cacheExtra;
-
-    return [plist writeToFile:path atomically:YES];
-}
-
 int main(int argc, char *argv[])
 {
-    @autoreleasepool {
-
-        /*
-         * LaunchDaemon 可能早于 MobileGestalt cache 完全可访问。
-         * 不退出，短时间重试。
-         *
-         * 最多等待约 30 秒。
-         */
-        for (int i = 0; i < 30; i++) {
-
-            if (enableDynamicIsland()) {
-                break;
-            }
-
-            sleep(1);
-        }
-    }
-
     return 0;
 }
